4d1696ad6e06c1c4584750c508a0ae3b263f14eb
geek_inside <38232156+bizvip@users.noreply.github.com>
2024-06-02 01:23:27 +0700

    init

--- src/layout/components/mixed/top-menu.vue
+++ src/layout/components/mixed/top-menu.vue
@@ -1,12 +1,4 @@
-<!--
- - MineAdmin is committed to providing solutions for quickly building web applications
- - Please view the LICENSE file that was distributed with this source code,
- - For the full copyright and license information.
- - Thank you very much for using MineAdmin.
- -
- - @Author X.Mo<root@imoi.cn>
- - @Link   https://gitee.com/xmo/mineadmin-vue
--->
+
 <template>
   <a-layout-content class="sys-menus">
     <a-menu ref="MaMenuRef" mode="horizontal" class="layout-banner-menu hidden lg:flex" :popup-max-height="360" :selected-keys="actives">
--- src/views/setting/code/components/settingComponent.vue
+++ src/views/setting/code/components/settingComponent.vue
@@ -1,12 +1,4 @@
-<!--
- - MineAdmin is committed to providing solutions for quickly building web applications
- - Please view the LICENSE file that was distributed with this source code,
- - For the full copyright and license information.
- - Thank you very much for using MineAdmin.
- -
- - @Author X.Mo<root@imoi.cn>
- - @Link   https://gitee.com/xmo/mineadmin-vue
--->
+
 <template>
   <a-modal v-model:visible="visible" :on-before-ok="save" width="600px" draggable top="50px" :align-center="false">
     <template #title>设置组件 - {{ row?.column_comment }}</template>
