608367eb4a6e0ec3e1c6e3e5a6502da17203cf5a
geek_inside <38232156+bizvip@users.noreply.github.com>
2024-06-02 20:49:15 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ router.beforeEach(async (to, from, next) => {
       return
     }
 
-    if (! userStore.user && userStore.user == undefined ) {
+    if (! userStore.user && userStore.user === undefined ) {
       const data = await userStore.requestUserInfo()
       data && next({ path: to.path, query: to.query })
     } else {
